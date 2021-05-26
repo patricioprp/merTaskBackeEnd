6b43a2b7e7c03619d19d4b44d3454b04508f2090
Patricio <patricioprp06@gmail.com>
2021-05-25 21:45:32 -0300

    cambios en las rutas

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,7 @@ const authController = require('../controllers/authController');
 
 //Iniciar sesion
 //  api/auth
-router.post('/',[
-    check('email','Agrega un email valido').isEmail(),
-    check('password','El password debe ser minimo de 6 caracteres').isLength({min:6}) 
-],
+router.post('/',
    authController.autenticarUsuario
 );
 
